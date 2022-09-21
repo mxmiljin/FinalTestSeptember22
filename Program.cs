02287ea089de2d8ff1abfe8b71b16e6ae01f4097
mxmiljin <81637481+mxmiljin@users.noreply.github.com>
2022-09-21 14:21:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {    
+        Console.Write("Введите слова или символы, разделенные запятой: ");
+        string inputData = Console.ReadLine();
+        string noSpaceData = inputData.Replace(" ", "");
+        string[] DataArray = noSpaceData.Split(",");
+
+        for (int i = 0; i < DataArray.Length; i++)
+        {
+            if (DataArray[i].Length <= 3)
+            { 
+                Console.Write(DataArray[i] + " ");
+            }
+        }
+    }
+}
